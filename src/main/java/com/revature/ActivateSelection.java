4b730a6a2faa9cf57f60f4a6503a1eddb4ef4daf
CellarDoor11 <Vetruba00@gmail.com>
2020-08-18 21:47:10 -0500

    added logging and another junit test

--- src/main/java/com/revature/ActivateSelection.java
+++ src/main/java/com/revature/ActivateSelection.java
@@ -1,21 +1,28 @@
 package com.revature;
 
 import java.io.IOException;
+import java.util.logging.FileHandler;
+
 /**
- * ActivateSelection takes the user's choice of menu input and activates the corresponding classes.
- * @param s is their selection which corresponds to the integer output from MainMenu class
+ * ActivateSelection takes the user's choice of menu input and activates the
+ * corresponding classes.
+ * 
+ * @param s is their selection which corresponds to the integer output from
+ *          MainMenu class
  */
 public class ActivateSelection {
     private int selection;
     private String dateInput;
     private String keywordInput;
+    java.util.logging.Logger logger = java.util.logging.Logger.getLogger(this.getClass().getName());
 
     public ActivateSelection(int s) {
         selection = s;
     }
 
     /**
-     * activates the selection from the menu and will run the menu again if the input was invalid
+     * activates the selection from the menu and will run the menu again if the
+     * input was invalid
      */
     public void activate() {
         if (selection == 1) {
@@ -24,8 +31,10 @@ public class ActivateSelection {
             entryObj.assembleEntry();
         }
         if (selection == 2) {
-            System.out.println("Please enter the date you would like to view entries from in this format (without parentheses): (yyyy-MM-dd). ");
-            System.out.println("Or, to view all entries in a time window, enter two dates in this format (without parentheses): (yyyy-MM-dd,yyyy-MM-dd).");
+            System.out.println(
+                    "Please enter the date you would like to view entries from in this format (without parentheses): (yyyy-MM-dd). ");
+            System.out.println(
+                    "Or, to view all entries in a time window, enter two dates in this format (without parentheses): (yyyy-MM-dd,yyyy-MM-dd).");
             try {
                 dateInput = new JournalInput().queryInput();
                 DateChecker checkerObj = new DateChecker(dateInput);
@@ -43,7 +52,7 @@ public class ActivateSelection {
             } else if (dateInput.length() == 21) {
                 LookUp searchObj = new LookUp(checkerObj.getDate1(), checkerObj.getDate2());
                 searchObj.lookInTable();
-            }            
+            }
         }
         if (selection == 3) {
             System.out.println("Please enter the keyword phrase you would like to search for:");
@@ -65,6 +74,17 @@ public class ActivateSelection {
             MainMenu anotherMenu = new MainMenu();
             int anotherSelection = anotherMenu.querySelection();
             new ActivateSelection(anotherSelection).activate();
+        }
+
+        logger.setUseParentHandlers(false);
+        try {
+            FileHandler fileHandler = new FileHandler("status.log");
+            logger.addHandler(fileHandler);
+            logger.info(java.time.LocalTime.now() + " App running");
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/test/java/com/revature/SqlTest.java
+++ src/test/java/com/revature/SqlTest.java
@@ -2,14 +2,77 @@ package com.revature;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.After;
 import org.junit.Test;
 
 public class SqlTest {
-    
 
     @Test
     public void checkTableExists() {
         SqlOperation sqlObj = new SqlOperation();
         assertTrue(sqlObj.checkTable());
+    }
+
+    @Test
+    public void enterEntryTest() {
+        ConnectDB db = new ConnectDB();
+        Connection connection = db.getConnection();
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter sqlDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String sqlFormattedDateTime = dateTime.format(sqlDateFormat);
+        SeparateDateTime separatorObj = new SeparateDateTime(sqlFormattedDateTime);
+        String date = separatorObj.getDate();
+        String time = separatorObj.getTime();
+        String sql1 = "INSERT INTO \"journal_table\" VALUES ('" + sqlFormattedDateTime + "','" + date + "','" + time + "','jUnit test entry');";
+        
+        PreparedStatement statement;
+
+        try {
+            statement = connection.prepareStatement(sql1);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+
+        String sql2 = "SELECT * FROM \"journal_table\" WHERE dateTime= '" + sqlFormattedDateTime + "';";
+
+        try {
+            statement = connection.prepareStatement(sql2);
+            ResultSet result = statement.executeQuery();
+            assertTrue(result.getString("date_and_time").equals(sqlFormattedDateTime));
+            assertTrue(result.getString("date_only").equals(date));
+            assertTrue(result.getString("time_only").equals(time));
+            assertTrue(result.getString("entry").equals("jUnit test entry"));
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        db.close();
+    }
+
+    @After
+    public void deleteTestEntry() {
+        ConnectDB db = new ConnectDB();
+        Connection connection = db.getConnection();
+        String sql = "DELETE FROM \"journal_table\" WHERE entry= 'jUnit test entry'";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        db.close();
     }
 }
